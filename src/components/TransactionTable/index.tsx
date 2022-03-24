bd9924e4d1c68dfc3b8c70b8d4c3aad7f9bde3a1
Perycles t Vieira Junior <peryclestvieirajunior@Perycless-MacBook-Pro.local>
2022-03-24 19:33:26 -0300

    create consume API tansactions simulate

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -34,9 +34,14 @@ export function TransactionTable(){
                             
                          <tr key={transaction.id}>
                                 <td>{transaction.title}</td>
-                                <td className={transaction.type}>{transaction.amount}</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR',{
+                                        style:'currency', 
+                                        currency: 'BRL'    
+                                    }).format( transaction.amount)}</td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createAt}</td>
+                                <td>{new Intl.DateTimeFormat('pt-BR')
+                                        .format(new Date(transaction.createAt))}</td>
                             </tr>
                         ))}
                        
--- src/index.tsx
+++ src/index.tsx
@@ -2,30 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { createServer,Model } from 'miragejs';
+import { makeServer } from './server';
 
 
+if(process.env.NODE_ENV !== 'production'){
+  makeServer();
+}
 createServer({
   models:{
     transaction: Model,
   },
   seeds(server){
+    server.db.emptyData();
+    
     server.db.loadData({
       transactions:[
         {
         id:1,
           title:'Freelance de website',
           type:'deposit',
-          value:6000,
+          amount:6000,
           category:'Dev',
-          createAt: new Date(),
+          createAt: new Date('2022-02-02 14:00:00'),
         },
         {
           id:2,
           title:'Aluguel',
           type:'withdraw',
-          value:100,
+          amount:1100,
           category:'Casa',
-          createAt: new Date(),
+          createAt: new Date('2022-03-10 14:00:00'),
         },
       ]
     })
@@ -38,6 +44,7 @@ createServer({
 
     this.post('/transactions', (schema, request)=>{
       const data = JSON.parse(request.requestBody);
+      
       return schema.create('transaction',data);
     })
   }
